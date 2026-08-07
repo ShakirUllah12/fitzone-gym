153b3c64afe37045fe0a829bd9b7347d6e0e1bd9
ShakirUllah12 <shakirkhangadooni@gmail.com>
2026-08-07 13:05:38 +0500

    add robots.txt and sitemap.xml for SEO

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ScrollToTop from './ScrollToTop.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout.jsx';
 import Home from './pages/Home.jsx';
@@ -11,6 +12,7 @@ import Contact from './pages/Contact.jsx';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
